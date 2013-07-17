b3750772bd6cad8cd5900742e0667c187734948a
Karsten Schmidt <k@postspectacular.com>
2013-07-17 20:15:41 +0100

    initial import

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject thi.ng/ldk-redis "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [thi.ng/ldk-core "0.1.0-SNAPSHOT"]
+                 [com.taoensso/carmine "2.0.0-RC1"]])
---
+++ test/thing_ld_redis/core_test.clj
@@ -0,0 +1,7 @@
+(ns thing-ld-redis.core-test
+  (:use clojure.test
+        thing-ld-redis.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
